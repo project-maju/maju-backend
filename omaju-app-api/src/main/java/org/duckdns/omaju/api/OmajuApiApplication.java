9d4ece6d7f24fe8614c7130c9ff80616e0425926
rigizer <rigizer@gmail.com>
2024-06-21 17:35:35 +0900

    [SETTING] 멀티모듈 프로젝트 설정으로 인한 컴포넌트 스캔 애노테이션 추가

--- omaju-app-api/src/main/java/org/duckdns/omaju/api/OmajuApiApplication.java
+++ omaju-app-api/src/main/java/org/duckdns/omaju/api/OmajuApiApplication.java
@@ -2,8 +2,11 @@ package org.duckdns.omaju.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("org.duckdns.omaju.core")
+@ComponentScan("org.duckdns.omaju.api")
 public class OmajuApiApplication {
 
 	public static void main(String[] args) {
--- omaju-app-batch/src/main/java/org/duckdns/omaju/batch/OmajuBatchApplication.java
+++ omaju-app-batch/src/main/java/org/duckdns/omaju/batch/OmajuBatchApplication.java
@@ -2,8 +2,11 @@ package org.duckdns.omaju.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("org.duckdns.omaju.core")
+@ComponentScan("org.duckdns.omaju.batch")
 public class OmajuBatchApplication {
 
 	public static void main(String[] args) {

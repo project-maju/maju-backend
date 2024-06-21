d279cac9f62b8b83213c5c81291d72c1c395f88d
rigizer <rigizer@gmail.com>
2024-06-21 17:32:17 +0900

    [SETTING] API 모듈의 test 패키지에 대한 application.yml 설정

--- omaju-app-api/src/test/java/org/duckdns/omaju/api/OmajuApiApplicationTests.java
+++ omaju-app-api/src/test/java/org/duckdns/omaju/api/OmajuApiApplicationTests.java
@@ -2,8 +2,10 @@ package org.duckdns.omaju.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class OmajuApiApplicationTests {
 
 	@Test

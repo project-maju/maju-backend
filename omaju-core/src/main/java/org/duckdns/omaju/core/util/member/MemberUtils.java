1b5310dca4ab1e9ac1a872f952928136a0d68307
rigizer <rigizer@gmail.com>
2024-07-04 11:28:07 +0900

    [MOD] Redis를 이용하여 사용자 정보를 저장하기 위한 유틸리티 구현

--- omaju-core/src/main/java/org/duckdns/omaju/core/util/member/MemberUtils.java
+++ omaju-core/src/main/java/org/duckdns/omaju/core/util/member/MemberUtils.java
@@ -1,2 +1,26 @@
-package org.duckdns.omaju.core.util.member;public class MemberUtils {
+package org.duckdns.omaju.core.util.member;
+
+import org.duckdns.omaju.core.entity.member.Member;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class MemberUtils {
+
+    public static Boolean hasNHoursElapsed(long pastEpochSeconds, int hours) {
+        Instant pastTime = Instant.ofEpochSecond(pastEpochSeconds);
+        ZoneId koreaZoneId = ZoneId.of("Asia/Seoul");
+        ZonedDateTime pastTimeKorea = ZonedDateTime.ofInstant(pastTime, koreaZoneId);
+        ZonedDateTime nowKorea = ZonedDateTime.now(koreaZoneId);
+
+        long hoursElapsed = ChronoUnit.HOURS.between(pastTimeKorea, nowKorea);
+
+        return hoursElapsed >= hours;
+    }
+
+    public static String getRedisKeyForMember(Member member) {
+        return member.getEmail() + " " + member.getProvider();
+    }
 }

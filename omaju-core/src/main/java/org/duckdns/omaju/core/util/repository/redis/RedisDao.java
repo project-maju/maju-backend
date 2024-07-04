c5df9efd4ca93733a39fed1b218d4599659ccd21
rigizer <rigizer@gmail.com>
2024-07-04 10:44:42 +0900

    [ADD] Redis 통신을 위한 DAO 추가

---
+++ omaju-core/src/main/java/org/duckdns/omaju/core/util/repository/redis/RedisDao.java
@@ -0,0 +1,40 @@
+package org.duckdns.omaju.core.util.repository.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@EnableRedisRepositories
+@RequiredArgsConstructor
+public class RedisDao {
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public void setValues(String key, String data) {
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        values.set(key, data);
+    }
+
+    public void setValues(String key, String data, Duration duration) {
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        values.set(key, data, duration);
+    }
+
+    public void setValues(String key, String data, Long expiration, TimeUnit timeUnit) {
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        values.set(key, data, expiration, timeUnit);
+    }
+    public String getValues(String key) {
+        ValueOperations<String, String> values = redisTemplate.opsForValue();
+        return values.get(key);
+    }
+
+    public void deleteValues(String key) {
+        redisTemplate.delete(key);
+    }
+}

e9b2abf79eaacaaaa196d790962d25b1d9bce1ec
rigizer <rigizer@gmail.com>
2024-07-13 13:04:21 +0900

    #16 [FEAT] TMap API를 이용한 도보 경로 탐색 API 구현

--- omaju-app-api/src/main/java/org/duckdns/omaju/api/controller/WalkingController.java
+++ omaju-app-api/src/main/java/org/duckdns/omaju/api/controller/WalkingController.java
@@ -32,4 +32,19 @@ public class WalkingController {
             ,@Parameter(name = "lon", description = "경도") @PathVariable double lon) {
         return walkingService.walkingTrails(lat, lon);
     }
+
+    @GetMapping("/tmap-trace/{startLat}/{startLon}/{endLat}/{endLon}")
+    @Operation(summary = "T MAP을 이용한 보행자 경로 안내", description = "위도/경도 기반 도보 경로 안내")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "정상적으로 데이터가 조회되는 경우"),
+            @ApiResponse(responseCode = "400", description = "유효하지 않은 access token 값 입력시 오류"),
+            @ApiResponse(responseCode = "401", description = "유효하지 않은 토큰으로 요청할 경우(responseBody에서 분기 처리되지 않습니다.)"),
+    })
+    public DataResponseDTO<?> tmapTrace(
+            @Parameter(name = "startLat", description = "시작 위도") @PathVariable double startLat
+            ,@Parameter(name = "startLon", description = "시작 경도") @PathVariable double startLon
+            ,@Parameter(name = "endLat", description = "종료 위도") @PathVariable double endLat
+            ,@Parameter(name = "endLon", description = "종료 경도") @PathVariable double endLon) {
+        return walkingService.tmapTrace(startLat, startLon, endLat, endLon);
+    }
 }

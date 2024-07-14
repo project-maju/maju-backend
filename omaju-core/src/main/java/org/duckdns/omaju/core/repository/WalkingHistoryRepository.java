8381672b1c4048ff68309f4affdc9db066b09d63
rigizer <rigizer@gmail.com>
2024-07-14 12:53:51 +0900

    #16 [FEAT] WalkingHistory JPQL 오탈자 수정 (테이블 이름 카멜케이스 변경)

--- omaju-core/src/main/java/org/duckdns/omaju/core/repository/WalkingHistoryRepository.java
+++ omaju-core/src/main/java/org/duckdns/omaju/core/repository/WalkingHistoryRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 @Repository
 public interface WalkingHistoryRepository extends JpaRepository<WalkingHistory, Integer> {
-    @Query("SELECT wh FROM walking_history wh WHERE wh.member.id = :memberId AND wh.created_at BETWEEN :startOfDayMillis AND :endOfDayMillis")
+    @Query("SELECT wh FROM WalkingHistory wh WHERE wh.member.id = :memberId AND wh.createdAt BETWEEN :startOfDayMillis AND :endOfDayMillis")
     List<WalkingHistory> findByMemberIdAndDateRange(int memberId, long startOfDayMillis, long endOfDayMillis);
 }
